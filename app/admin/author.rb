0bd943315809944f912a479dedc285a858381deb
josenauto <jemilionautoch@gmail.com>
2015-12-06 21:38:42 -0300

    Add a sidebar for the Author

--- app/admin/author.rb
+++ app/admin/author.rb
@@ -1,5 +1,11 @@
 ActiveAdmin.register Author do
 
+    sidebar 'Books by this Author', :only => :show do
+      table_for Book.joins(:author).where(:author_id => author.id) do |t|
+        t.column("Title") { |book| book.name }
+      end
+    end
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
