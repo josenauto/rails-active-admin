b19a4da5f4f3a706e096b46e41e554b91996a348
josenauto <jemilionautoch@gmail.com>
2015-12-10 16:07:50 -0300

    Add section in the dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -3,11 +3,21 @@ ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
 
   content title: proc{ I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
+  #  div class: "blank_slate_container", id: "dashboard_default_message" do
+  #    span class: "blank_slate" do
+  #      span I18n.t("active_admin.dashboard_welcome.welcome")
+  #      small I18n.t("active_admin.dashboard_welcome.call_to_action")
+  #    end
+  #  end
+
+    section "Recent Books" do
+      table_for Book.order("name desc").limit(5) do
+        column :name do |book|
+          link_to book.name, admin_book_path(book)
+        end
+        column :price
       end
+      strong { link_to "View all books", admin_books_path }
     end
 
     # Here is an example of a simple dashboard with columns and panels.
